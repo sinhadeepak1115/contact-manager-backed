b29bd201824f964dc5642201de05b292bb0dc53e
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-19 20:54:52 +0530

    checking error handling in project.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,43 +1,49 @@
+const asyncHandler = require("express-async-handler")
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async(req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create new contacts
 //@route POST /api/contacts
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async(req, res) => {
   console.log("The request body is", req.body);
+  const {name, email, phone} = req.body;
+  if(!name || !email || !phone){
+    res.status(400);
+    throw new Error("All Fields are mandatory.")
+  }
   res.status(201).json({ message: "Create Contact" });
-};
+});
 
 //@desc Get indivisual contact
 //@route GET /api/contact/:id
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async(req, res) => {
   res.status(200).json({ message: `Get contact of ${req.params.id}` });
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async(req, res) => {
   res.status(200).json({ message: `Update contact of ${req.params.id}` });
-};
+});
 
 //@desc Delete contact
 //@route  DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async(req, res) => {
   res.status(200).json({ message: `Delete contact of ${req.params.id}` });
-};
+});
 module.exports = {
   getContacts,
   createContact,
