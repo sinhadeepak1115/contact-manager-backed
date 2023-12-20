d385b06fa91ad85af8cdcb9c5be98e570fc2e840
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-21 00:46:06 +0530

    user control is completed with hashing.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,37 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 //@desc Register a user 
 //@route POST /api/users/register
 //@access public
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.json({ message: "Register the user"});
+    const {username, email, password} = req.body;
+  if(!username || !email || !password){
+    res.status(400);
+    throw new Error("All Fields are mandatory.")
+  }
+  const userAvailable = await User.findOne({email});
+  if (userAvailable){
+    res.status(400);
+    throw new Error("User already registered!")
+  }
+  //Hash Password
+  const hashedPassword = await bcrypt.hash(password,10);
+  console.log("Hashed Password: ", hashedPassword);
+  const user =  await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log("User Created", user);
+  if (user){
+    res.status(201).json({_id:user.id, email: user.email});
+  }else{
+    res.status(400);
+    throw new Error("User data us not valid");
+  }
+  res.json({ message: "Register the user"});
 });
 
 //@desc Login user 
